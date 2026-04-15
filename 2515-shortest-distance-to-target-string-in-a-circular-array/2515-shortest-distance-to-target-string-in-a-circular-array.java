7f1d924034e1f7d9ebe2302a1d5d25a4e7c897c0
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-04-15 14:41:42 +0530

    Time: 0 ms (100%), Space: 47 MB (28.57%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -3,16 +3,13 @@ class Solution {
         int temp=Integer.MAX_VALUE,n=words.length;
         int i=I,j=I;
         if(words[I].equals(target))return 0;
-        for(int count=0;count<words.length;count++){
-            if(words[i].equals(target)){
-                temp=Math.min((i - I + n) % n,temp);
+        for(int c=0;c<words.length;c++){
+            if(words[c].equals(target)){
+            int clock=(c-I+n)%n;
+            int anti=(I-c+n)%n;
+            temp=Math.min(temp,Math.min(clock,anti));
             }
-            i=(i+1)%n;
-        
-            if(words[j].equals(target)){
-                temp=Math.min((I - j + n) % n,temp);
-            }
-            j=(j-1+n)%n;
+            
         }
 
         if(temp==Integer.MAX_VALUE)return -1;
