a3aa981b1b6e4ad747a58e98bb2de904ed61cecf
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-04-19 13:39:11 +0530

    Time: 1 ms (100%), Space: 46.3 MB (84.72%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,25 +1,22 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        Map<Integer,Integer> map=new HashMap<>();
-        Map<Integer,Integer> map2=new HashMap<>();
         int n=nums.length;
-        map.put(0,nums[0]);
-        map2.put(n-1,nums[n-1]);
-        
-        int a=nums[0];
-        int b=nums[nums.length-1];
-        for(int i=1;i<nums.length;i++){
-            a=Math.max(a,nums[i]);
-            b=Math.min(b,nums[n-i-1]);
-            map.put(i,a);
-            map2.put(n-i-1,b);
+        int[] pre=new int[n];
+        int[] suf=new int[n];
+        pre[0]=nums[0];
+        suf[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            pre[i]=Math.max(pre[i-1],nums[i]);
+            suf[n-i-1]=Math.min(suf[n-i],nums[n-i-1]);
         }
-        int ans=0,idx=-1;
+       
         for(int i=0;i<n;i++){
-            if(map.get(i)-map2.get(i)<=k){
+            int diff=pre[i]-suf[i];
+            if(diff<=k){
                 return i;
             }
         }
         return -1;
+
     }
 }
