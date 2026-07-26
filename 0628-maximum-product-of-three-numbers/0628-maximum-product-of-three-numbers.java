05c209977b510048c5ffc62bcfcc156e8354bd4b
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-07-26 15:11:31 +0530

    Time: 3 ms (86.33%), Space: 47 MB (97.34%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -26,8 +26,6 @@ class Solution {
                 min2=nums[i];
             }
         }
-        int p1 = min1 * min2 * max1;
-        int p2 = max1 * max2 * max3;
-        return Math.max(p1, p2);
+    return Math.max(max1 * max2 * max3, max1 * min1 * min2);
     }
 }
