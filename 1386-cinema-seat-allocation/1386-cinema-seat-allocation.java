f7fbe35fd7b16654f30620d740fb0a12dab85608
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-08-19 12:13:35 +0530

    Time: 32 ms (35%), Space: 52.7 MB (48.16%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -8,27 +8,59 @@ class Solution {
                .add(seatNumber);
          } 
         int ans=0;  
-        for(int i:map.keySet()){
-            boolean group1=false;
-            boolean group2=false;
-            boolean group3=false;
-            Set<Integer> set = map.get(i);
-            if(!set.contains(2) && !set.contains(3) && !set.contains(4) && !set.contains(5)){
-                group1=true;
-            }
-            if(!set.contains(6) && !set.contains(7) && !set.contains(8) && !set.contains(9)){
-               group3=true;
-            }
-            if(!set.contains(4) && !set.contains(5) && !set.contains(6) && !set.contains(7)){
-                group2=true;
-            }
-            if(group1 && group3){
-               ans+=2;
-            }
-            else if(group1|| group2 || group3){
-               ans+=1;
+        for (Set<Integer> set : map.values()) {
+
+            boolean left =
+                    !set.contains(2) &&
+                    !set.contains(3) &&
+                    !set.contains(4) &&
+                    !set.contains(5);
+
+            boolean middle =
+                    !set.contains(4) &&
+                    !set.contains(5) &&
+                    !set.contains(6) &&
+                    !set.contains(7);
+
+            boolean right =
+                    !set.contains(6) &&
+                    !set.contains(7) &&
+                    !set.contains(8) &&
+                    !set.contains(9);
+
+            if (left && right) {
+                ans += 2;
+            } else if (left || middle || right) {
+                ans += 1;
             }
         }
+
+
+
+
+
+
+        // for(int i:map.keySet()){
+        //     boolean group1=false;
+        //     boolean group2=false;
+        //     boolean group3=false;
+        //     Set<Integer> set = map.get(i);
+        //     if(!set.contains(2) && !set.contains(3) && !set.contains(4) && !set.contains(5)){
+        //         group1=true;
+        //     }
+        //     if(!set.contains(6) && !set.contains(7) && !set.contains(8) && !set.contains(9)){
+        //        group3=true;
+        //     }
+        //     if(!set.contains(4) && !set.contains(5) && !set.contains(6) && !set.contains(7)){
+        //         group2=true;
+        //     }
+        //     if(group1 && group3){
+        //        ans+=2;
+        //     }
+        //     else if(group1|| group2 || group3){
+        //        ans+=1;
+        //     }
+        // }
         return ans+2*(n-map.size());
     }
 }
