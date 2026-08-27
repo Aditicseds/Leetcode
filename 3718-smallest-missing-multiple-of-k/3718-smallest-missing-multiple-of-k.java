450bca5eea85dfe6a2eac13d7098fb59997642ac
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-08-27 17:12:22 +0530

    Time: 2 ms (69.48%), Space: 45.6 MB (15.78%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,27 +1,27 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        // Set<Integer> set=new HashSet<>();
-        // for(int num:nums){
-        //     set.add(num);
-        // }
-        // for(int multiple=k;;multiple+=k){
-        //         if(!set.contains(multiple))
-        //             return multiple;
-        // }
-         for (int multiple = k; ; multiple += k) {
-            boolean found = false;
-
-            for (int num : nums) {
-                if (num == multiple) {
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                return multiple;
-            }
+        Map<Integer,Integer> set=new HashMap<>();
+        for(int num:nums){
+            set.put(num,1);
         }
+        for(int multiple=k;;multiple+=k){
+                if(!set.containsKey(multiple))
+                    return multiple;
+        }
+        //  for (int multiple = k; ; multiple += k) {
+        //     boolean found = false;
+
+        //     for (int num : nums) {
+        //         if (num == multiple) {
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+
+        //     if (!found) {
+        //         return multiple;
+        //     }
+        // }
     
     }
 }
