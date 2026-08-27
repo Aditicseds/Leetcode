1d51115693af59c9f9f903d2e11e147462d8e5c9
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-08-27 16:56:28 +0530

    Time: 6 ms (99.7%), Space: 48.3 MB (15.86%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,4 +1,10 @@
 class Solution {
+    long getHours(long hour,int mid,int[] piles){
+            for(int pile:piles){
+                hour=hour+(pile+mid-1)/mid;
+            }
+            return hour;
+    }
     public int minEatingSpeed(int[] piles, int h) {
         int ans=0;
         int n=piles.length;
@@ -10,10 +16,7 @@ class Solution {
         
         while(f<=l){
             int mid=(f+l)/2;
-            long hours=0;
-            for(int pile:piles){
-                hours=hours+(pile+mid-1)/mid;
-            }
+            long hours=getHours(0,mid,piles);
             if(hours<=h){
               l=mid-1;
               ans=mid;
