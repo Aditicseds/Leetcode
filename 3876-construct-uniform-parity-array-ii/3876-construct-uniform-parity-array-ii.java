d1b82652870f4ba9e1bd6229a5405cc9d388553d
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-09-03 12:15:58 +0530

    Time: 5 ms (83.33%), Space: 121.5 MB (81.16%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,18 +1,11 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-       List<Integer> odd = new ArrayList<>();
-       List<Integer> even = new ArrayList<>();
+       boolean even = true;
        int min=Integer.MAX_VALUE;
        for(int n : nums1){
-        if(n%2==0){
-            even.add(n);
-        }
-        else{
-            odd.add(n);
-        }
+        if(n%2!=0) even = false;
         min=Math.min(min,n);
        }
-       if(odd.size()==0 || even.size()==0)return true;
-       return min%2==1;
+       return min%2==1 || even;
     }
 }
