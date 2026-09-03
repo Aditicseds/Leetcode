f03a1aaa38c6935c5f593080a4257eda5cd279df
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-09-03 16:26:21 +0530

    Time: 98 ms (51.18%), Space: 47.9 MB (75.35%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -1,31 +1,38 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
         int ones=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='1')ones++;
+        int i=0,j=0;
+        int ans=0;
+        for(char c: s.toCharArray()){
+            if(c=='1'){
+               ones++;
+            }
         }
-        if(ones==0)return 0;
-        List<Integer> cnt = new ArrayList<>();
-        int i=0;
-        while(i<s.length()){
-            if(s.charAt(i)=='0'){
-               int idx=i;
-               while(i<s.length() && s.charAt(i)=='0'){
-                    i++;
-                    
+        int prev=0,next=0;
+        while(j<s.length()){
+            if(s.charAt(j)=='1'){
+                int start=j;
+                while(start<s.length() && s.charAt(start)=='1'){
+                    start++;
                 }
-                cnt.add(i-idx);
+                if(start==s.length()) break;
+                next=0;
+                while(start<s.length() && s.charAt(start)=='0'){
+                    next++;
+                    start++;
+                }
+                if(prev>0 && next>0){
+                    i=Math.max(i,prev+next);
+                }
+                prev=next;
+                j=start;
 
             }
             else{
-                i++;
+                prev++;
+                j++;
             }
-         
         }
-        int ans=0;
-        for(int j=1;j<cnt.size();j++){
-            ans=Math.max(ans,cnt.get(j)+cnt.get(j-1));
-        }
-        return ans+ones;
+        return i+ones;
     }
 }
