ecae173451191b141e51bbad93085724cb08891b
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-09-03 16:30:23 +0530

    Time: 56 ms (84.6%), Space: 48.1 MB (31.8%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -1,38 +1,31 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        int ones=0;
-        int i=0,j=0;
-        int ans=0;
-        for(char c: s.toCharArray()){
-            if(c=='1'){
-               ones++;
+        int ones = 0;
+        int maxGain = 0;
+        int prevZero = 0;
+        int j = 0;
+        while (j < s.length()) {
+            if (s.charAt(j) == '0') {
+                prevZero++;
+                j++;
+                continue;
             }
-        }
-        int prev=0,next=0;
-        while(j<s.length()){
-            if(s.charAt(j)=='1'){
-                int start=j;
-                while(start<s.length() && s.charAt(start)=='1'){
-                    start++;
-                }
-                if(start==s.length()) break;
-                next=0;
-                while(start<s.length() && s.charAt(start)=='0'){
-                    next++;
-                    start++;
-                }
-                if(prev>0 && next>0){
-                    i=Math.max(i,prev+next);
-                }
-                prev=next;
-                j=start;
-
-            }
-            else{
-                prev++;
+            while (j < s.length() && s.charAt(j) == '1') {
+                ones++;
                 j++;
             }
+            int nextZero = 0;
+            while (j < s.length() && s.charAt(j) == '0') {
+                nextZero++;
+                j++;
+            }
+            if (prevZero > 0 && nextZero > 0) {
+                maxGain = Math.max(maxGain, prevZero + nextZero);
+            }
+
+            prevZero = nextZero;
         }
-        return i+ones;
+
+        return ones + maxGain;
     }
 }
