e406e8ee688d7a55f5fb5e51a88c2bb654bca7f6
Aditi Kaushik <133771038+Aditicseds@users.noreply.github.com>
2026-09-04 11:37:08 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (49.36%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,22 +1,21 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int n=nums.length;
-        int[] pre=new int[n];
-        int[] suf=new int[n];
-        pre[0]=nums[0];
-        suf[n-1]=nums[n-1];
-        for(int i=1;i<n;i++){
-            pre[i]=Math.max(pre[i-1],nums[i]);
-            suf[n-i-1]=Math.min(suf[n-i],nums[n-i-1]);
+      int min[]  = new int[nums.length];
+      min[nums.length-1]=nums[nums.length-1];
+      int mini = nums[nums.length-1];
+      for(int i=nums.length-2;i>=0;i--){
+          if(nums[i]<mini){
+            mini=nums[i];
+          }
+          min[i]=mini;
+      }
+      int max=-2;
+      for(int i=0;i<nums.length;i++){
+        max=nums[i]>max?nums[i]:max;
+        if(max-min[i]<k || max-min[i]==k){
+            return i;
         }
-       
-        for(int i=0;i<n;i++){
-            int diff=pre[i]-suf[i];
-            if(diff<=k){
-                return i;
-            }
-        }
-        return -1;
-
+      }
+      return -1;
     }
 }
